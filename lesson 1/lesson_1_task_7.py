792130664d3716e6060b70de8bca767918347e61
dimak <dimik1986@gmail.com>
2024-01-20 00:47:02 +0400

    выполнил задания к первому уроку

---
+++ lesson 1/lesson_1_task_7.py
@@ -0,0 +1,31 @@
+def number_one():
+    print(1, end = "")
+
+def number_two():
+    print(2, end = "")
+
+def number_three():
+    print(3, end = "")
+
+def number_four():
+    print(4, end = "")
+
+def number_five():
+    print(5, end = "")
+
+def number_six():
+    print(6, end = "")
+
+def number_seven():
+    print(7, end = "")
+
+def number_eight():
+    print(8, end = "")
+
+def number_nine():
+    print(9, end = "")
+
+def number_zero():
+    print(0, end = "")
+
+number_eight(), number_eight(), number_zero(), number_zero(), number_five(), number_five(), number_five(), number_three(), number_five(), number_three(), number_five()
