d71359d5cf3120b46a30fa35610c8fda87f068fe
Kliuev Dmitrii <dimik1986@gmail.com>
2024-11-28 15:15:55 +0400

    reserch

--- lesson 8/lesson_8_task_1.py
+++ lesson 8/lesson_8_task_1.py
@@ -1,19 +1,34 @@
-from time import sleep
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 from pages.FormPage import FormPage
 
-def test_alert():
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
 
+@pytest.mark.parametrize("first_name, last_name, address, zip_code, city, country, mail, phone, job_position, company", [
+    ("Иван", "Петров", "Ленина, 55-3", "", "Москва", "Россия", "test@skypro.com", "+7985899998787", "QA", "SkyPro")
+])
+def test_empty_fild(browser, first_name, last_name, address, zip_code, city, country, mail, phone, job_position, company):
     form_page = FormPage(browser)
     form_page.get()
-    form_page.enter_value()
+    form_page.enter_first_name(first_name)
+    form_page.enter_last_name(last_name)
+    form_page.enter_address(address)
+    form_page.enter_zip(zip_code)
+    form_page.enter_city(city)
+    form_page.enter_country(country)
+    form_page.enter_mail(mail)
+    form_page.enter_phone(phone)
+    form_page.enter_job_position(job_position)
+    form_page.enter_company(company)
     form_page.click_button()
-    correct = form_page.is_correct_values()
-    not_correct = form_page.is_not_correct_values()
-    assert correct == not_correct
+    empty_fields = form_page.get_empty_fild()
     
-    browser.quit()
+    assert "zip-code" in empty_fields, "Поле 'Zip Code' не отмечено как пустое."
--- lesson 8/pages/FormPage.py
+++ lesson 8/pages/FormPage.py
@@ -7,28 +7,64 @@ class FormPage:
     def get(self):
         self._driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    def enter_value(self):
-        self._driver.find_element(By.CSS_SELECTOR, "[name='first-name']").send_keys("Иван")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='last-name']").send_keys("Петров")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='address']").send_keys("Ленина, 55-3")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='e-mail']").send_keys("test@skypro.com")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='phone']").send_keys("+7985899998787")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='city']").send_keys("Москва")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='country']").send_keys("Россия")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='job-position']").send_keys("QA")
-        self._driver.find_element(By.CSS_SELECTOR, "[name='company']").send_keys("SkyPro")
+    def enter_first_name(self, first_name):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='first-name']")
+        element.clear()
+        element.send_keys(first_name)
+    
+    def enter_last_name(self, last_name):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='last-name']")
+        element.clear()
+        element.send_keys(last_name)
+
+    def enter_address(self, address):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='address']")
+        element.clear()
+        element.send_keys(address)
+        
+    def enter_zip(self, zip):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='zip-code']")
+        element.clear()
+        element.send_keys(zip)
+
+    def enter_city(self, city):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='city']")
+        element.clear()
+        element.send_keys(city)
+
+    def enter_country(self, country):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='country']")
+        element.clear()
+        element.send_keys(country)
+
+    def enter_mail(self, mail):
+        element = self._driver.find_element(By.XPATH, "//input[@type='email' and @class='form-control' and @name='e-mail']")
+        element.clear()
+        element.send_keys(mail)
+
+    def enter_phone(self, phone):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='phone']")
+        element.clear()
+        element.send_keys(phone)
+
+    def enter_job_position(self, job_position):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='job-position']")
+        element.clear()
+        element.send_keys(job_position)
+
+    def enter_company(self, company):
+        element = self._driver.find_element(By.XPATH, "//input[@type='text' and @class='form-control' and @name='company']")
+        element.clear()
+        element.send_keys(company)
 
     def click_button(self):
         self._driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
-
-    def is_correct_values(self):
-        success = self._driver.find_elements(By.CSS_SELECTOR, ".alert-success")
-        for fields in success:
-            success = fields.find_elements(By.CSS_SELECTOR, ".alert-success")
-            return success
-
-    def is_not_correct_values(self):
-        danger = self._driver.find_elements(By.CSS_SELECTOR, ".alert-danger")
-        for fields in danger:
-            danger = fields.find_elements(By.CSS_SELECTOR, ".alert-danger")
-            return danger
+        
+    def get_empty_fild(self):
+       elements = self._driver.find_elements(By.XPATH, "//div[contains(@class, 'alert') and text()='N/A']")
+       empty_fields = {}
+       for elem in elements:
+            field_id = elem.get_attribute("id")
+            field_text = elem.text.strip()
+            empty_fields[field_id] = field_text
+       return empty_fields
